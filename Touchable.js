25112d02123235f2d2338450211e7b7520346ee7
Lukas1h <Lukas1h7@gmail.com>
2023-05-25 15:55:03 -0700

    v 11

--- Touchable.js
+++ Touchable.js
@@ -1,30 +1,25 @@
-import React, {useState} from 'react';
-import {requireNativeComponent,Text,View} from 'react-native';
-const KPPUnifiedButton = requireNativeComponent('KPPUnifiedButton');
+import React, { useState } from "react";
+import { requireNativeComponent, Text, View } from "react-native";
+const KPPUnifiedButton = requireNativeComponent("KPPUnifiedButton");
 
-const Touchable = ({onPress, title,children,style}) => {
-    const [flash, setFlash] = useState(false);
-
-
-
-    return (
-      <>
-        {flash && <View></View>}
-        <View
-          style={style}>
-          <KPPUnifiedButton
-            onClicked={() => {
-              onPress();
-            }}
-            style={{
-              padding: 8,
-              backgroundColor: 'transparent',
-            }}>
-            {children}
-          </KPPUnifiedButton>
-        </View>
-      </>
-    );
+const Touchable = ({ onPress, title, children, style }) => {
+  return (
+    <>
+      <View style={style}>
+        <KPPUnifiedButton
+          onClicked={() => {
+            console.log("Pressed!");
+            onPress();
+          }}
+          style={{
+            backgroundColor: "black",
+          }}
+        >
+          {children}
+        </KPPUnifiedButton>
+      </View>
+    </>
+  );
 };
 
 export default Touchable;
--- TouchableOpacity.js
+++ TouchableOpacity.js
@@ -1,31 +1,43 @@
-import React, {useState} from 'react';
-import {Text, View} from 'react-native';
-import Touchable from './Touchable.js';
+import React, { useState } from "react";
+import { Text, View } from "react-native";
+import Touchable from "./Touchable.js";
 
-const TouchableOpacity = ({onPress, children, style}) => {
+const TouchableOpacity = ({ onPress, children, style }) => {
   const [flash, setFlash] = useState(false);
 
   const handleFlash = () => {
     setFlash(true);
-    console.log('Flashing');
+    console.log("Flashing");
     setTimeout(() => {
       setFlash(false);
     }, 200);
   };
-  console.log('Flash is ', flash);
+  console.log("Flash is ", flash);
+
+  const flashStyle = flash ? { backgroundColor: "black" } : {};
+
+  const childrenWithFlashStyle = React.Children.map(children, (child) => {
+    return React.cloneElement(child, {
+      style: [child.props.style, flashStyle],
+    });
+  });
+
 
   return (
     <>
       <Touchable
-        style={style}
         onPress={() => {
           onPress();
           handleFlash();
         }}
-        style={{
-          backgroundColor: flash ? 'black' : 'transparent',
-        }}>
-        {children}
+        style={[
+          {
+            backgroundColor: flash ? "black" : "transparent",
+          },
+          style,
+        ]}
+      >
+        {flash ? childrenWithFlashStyle : children}
       </Touchable>
       {flash && <View></View>}
     </>
