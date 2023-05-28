6f6bd498efe88ce7c0ae2ac8716b88dd7331d4e9
Lukas1h <Lukas1h7@gmail.com>
2023-05-28 13:58:13 -0700

    Fixed

--- ScrollView.js
+++ ScrollView.js
@@ -3,20 +3,15 @@ import {
   requireNativeComponent,
 } from "react-native-kindle";
 const KPPScrollView = requireNativeComponent("KPPPaginatedScrollView");
+export default function ScrollView(props) {
+  const scrollViewStyle = {
+    width: "100%",
+    height: "92%",
+    borderWidth: 1,
+    ...props.style,
+  };
 
-export default function ScrollView(props){
-    return React.cloneElement(
-      KPPScrollView,
-      Object.assign(
-        {},
-        {
-          width: "100%",
-          height: "92%",
-          padding: "2%",
-          borderWidth: 1,
-        },
-        props.style
-      )
-    );
+  return (
+    <KPPScrollView style={scrollViewStyle}>{props.children}</KPPScrollView>
+  );
 }
-
